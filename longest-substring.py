a33d78004f2b24e2fc5f3b1bfa4e33dec5991128
Pedro Damasceno <pedro.d.vas@gmail.com>
2025-10-14 00:12:38 -0300

    add longest substring problem

--- longest-substring.py
+++ longest-substring.py
@@ -8,3 +8,39 @@ class Solution(object):
         repeating characters and returns its len
         """
         biggest = 0
+        beginning = 0
+        current_set = set()
+        for i in range(len(s)):
+            if s[i] in current_set:
+                while s[i] in current_set:
+                    current_set.remove(s[beginning])
+                    beginning += 1
+
+            current_set.add(s[i])
+            if i - beginning + 1 > biggest:
+                biggest = i - beginning + 1
+        
+        return biggest
+    
+    def lengthOfLongestSubstring0(self, s):
+        """
+        solução errada pois não avança 
+        corretamente quando encontra caractere 
+        repetido
+        """
+        biggest = 0
+        beginning = 0
+        current_set = set()
+        for i in range(len(s)):
+            if s[i] in current_set:
+                beginning = i
+                current_set = set()
+                current_set.add(s[i])
+                continue
+            
+
+            current_set.add(s[i])
+            if i - beginning + 1 > biggest:
+                biggest = i - beginning + 1
+        
+        return biggest
