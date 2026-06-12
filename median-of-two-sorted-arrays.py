c8f0d61d3000b05334f145047a2d023b282835f9
Pedro Damasceno Vasconcellos <pedro.d.vas@gmail.com>
2026-06-12 18:13:01 -0300

    Add heap and part of the operations to be used.

--- median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays.py
@@ -9,27 +9,41 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+        index1 = 0
+        index2 = 0
+        middle = len(nums1)/2 + len(nums2)/2
+        max_heap = Min_heap(nums1[0], nums1, nums2[0], nums2)
+        while index1 + 1 + index2 + 1 < middle:
+            index1, index2 = max_heap.update_list(index1, nums1, index2, nums2)
 
-class Max_heap3(object):
-    def __init__(self, val_A, list_A, val_B, list_B, val_C, list_C):
-        if val_A > val_B and val_A > val_C:
-            self.top = HeapNode(val_A, list_A)
-            self.left = HeapNode(val_B, list_B)
-            self.right = HeapNode(val_C, list_C)
+        
+        return max_heap.top
 
-        elif val_B > val_A and val_B > val_C:
-            self.top = HeapNode(val_B, list_B)
-            self.left = HeapNode(val_A, list_A)
-            self.right = HeapNode(val_C, list_C)
 
+
+
+class Min_heap(object):
+    def __init__(self, index_A, list_A, index_B, list_B):
+        if list_A[index_A] > list_B[index_B]:
+            self.top = HeapNode(list_A[index_A], list_A)
+            self.bottom = HeapNode(list_B[index_B], list_B)
+            
         else:
-            self.right = HeapNode(val_C, list_C)
-            self.top = HeapNode(val_A, list_A)
-            self.left = HeapNode(val_B, list_B)
-
+            self.top = HeapNode(list_B[index_B], list_B)
+            self.bottom = HeapNode(list_A[index_A], list_A)
+            
     def print(self):
-        print(f"top: {self.top.value} \nleft: {self.left.value} right: {self.right.value}")
+        print(f"top: {self.top.value} \nbottom: {self.bottom.value}")
 
+    def update_list(self, index_A, list_A, index_B, list_B):
+        if self.top.list == list_A:
+            new_value = list_A[index_A+1]
+        else:
+            new_value = list_B[index_B+1]
+
+        if new_value > self.bottom.value:
+            self.top = HeapNode(new_value, self.top.list)
+        
 
 class HeapNode(object):
     def __init__(self, value, list):
@@ -38,5 +52,5 @@ class HeapNode(object):
 
 
 if __name__ == "__main__":
-    test_heap = Max_heap3(1, [], 2, [], 3, [])
+    test_heap = Min_heap(1, [], 2, [], 3, [])
     test_heap.print()
