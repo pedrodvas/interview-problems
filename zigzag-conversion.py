1dbd02db22bf3ad22944f4d91aa3cc6e736794da
Pedro Damasceno Vasconcellos <pedro.d.vas@gmail.com>
2026-06-13 18:58:39 -0300

    Add fix of zigzag conversion. The correction was not commited before.

--- zigzag-conversion.py
+++ zigzag-conversion.py
@@ -5,28 +5,33 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
+        if numRows == 1:
+            return s
         #the zigzag repats itself every numRows+numRows-2
         #I will create a matrix to store that
         size_pattern = numRows+numRows-2
         #will be used when the line has not straight
         #lines in it
-        final_string = []
+        chars_list = []
         for i in range(numRows):
             even_occurence = i
             odd_occurence = size_pattern-i
-            while even_occurence < len(s) and odd_occurence < len(s):
-                final_string.append(s[even_occurence])
-                if odd_occurence != even_occurence:
-                    final_string.append(s[odd_occurence])
-                    print("hi")
+            skip_odd = True if i==0 else False
+            while even_occurence < len(s) or odd_occurence < len(s):
+                if even_occurence < len(s):
+                    chars_list.append(s[even_occurence])
+                if not skip_odd and odd_occurence != even_occurence and odd_occurence < len(s):
+                    chars_list.append(s[odd_occurence])
+
                 even_occurence += size_pattern
                 odd_occurence += size_pattern
-                print(final_string)
         
-        print(final_string)
+        final_string = "".join(chars_list)
+        return final_string
         
             
 
 if __name__ == "__main__":
     sol = Solution()
+    sol.convert("PAYPALISHIRING", 3)
     sol.convert("PAYPALISHIRING", 4)
