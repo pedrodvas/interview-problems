221ab17e3efcf3715d7c8e0091baff09bd3b9797
Pedro Damasceno <pedro.d.vas@gmail.com>
2026-06-20 20:19:16 -0300

    Add wrong solution temporarily.

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
@@ -12,23 +12,51 @@ class Solution:
         #here the graph is already initialized
         #now I am going to prepare the variables
         #that are going to be used in the modified BFS
-        flights_left = [None]*n
-        flights_left[src] = k
         #in the initial stack, I will put the neighbors to src
-        bfs_stack = []
+        bfs_queue = deque()
         #bfs elements will be (src, dst, price)
         #we need to have src to measure how many
         #flights were already taken before we got there
-        for i in range(len(flights_graph[src])):
-            bfs_stack.append((src, flights_graph[src][i][0], flights_graph[src][i][1]))
+        if k < 0:
+            return -1
+        for i in flights_graph[src]:
+            bfs_queue.append((src, i[0], i[1], k+1))
         
-        print(bfs_stack)
+        #you don't have to worry about going back to a point you've gone
+        #before, because, if this new path is the new smallest one, the bfs will
+        #eventually get to your destiny with the new cheapest combination
+
         #now we should start iterating through each node
         #using the bfs, and ignoring the nodes that have 
         #no trips left. The amount of trips left will be accessed
         #by the flights_left list.
 
+        smallest_prices = [float('inf')]*n
+
+        while bfs_queue:
+            curr_flight = bfs_queue.popleft()
+            curr_src = curr_flight[0]
+            curr_dst = curr_flight[1]
+            curr_price = curr_flight[2]
+            curr_left = curr_flight[3]
+            curr_left = curr_left - 1
+            if curr_left <= 0:
+                continue
+            if smallest_prices[curr_dst] < curr_price:
+                continue
+            smallest_prices[curr_dst] = curr_price
+            if curr_dst == dst:
+                continue
+            for i in flights_graph[curr_dst]:
+                bfs_queue.append((curr_dst, i[0], curr_price+i[1], curr_left))
+            
+
+        return smallest_prices[dst] if smallest_prices[dst] != float('inf') else -1
+
+        
+
 
 if __name__ == "__main__":
     sol = Solution()
-    sol.findCheapestPrice(n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1)
+    a = sol.findCheapestPrice(n = 4, flights = [[0,1,10],[0,2,50],[1,2,10],[2,3,10]], src = 0, dst = 3, k = 1)
+    print(a)
