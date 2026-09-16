5ee9546be1f8fd3b2fd95242f528070b95fecb4b
pedrodvas <pedro.d.vas@gmail.com>
2026-09-16 15:24:38 -0300

    Update solved problems.

--- solved-problems/reorganize-string.py
+++ solved-problems/reorganize-string.py
@@ -14,15 +14,19 @@ class Solution:
         letter_count_heap = []
         for i in letter_count_dict:
             heapq.heappush(letter_count_heap, [-letter_count_dict[i], i])
-        print(letter_count_heap)
         ret_list = []
         last = [1, None]
         for i in range(len(s)):
             print(letter_count_heap)
-            heapq.heappush(letter_count_heap, last)
             to_use = heapq.heappop(letter_count_heap)
+            if to_use[1] == None:
+                return ""
+            if last[0] != 0:
+                heapq.heappush(letter_count_heap, last)
+            print(f"to use is {to_use}")
             to_use[0] += 1
             ret_list.append(to_use[1])
+            print(f"appended char, now answer is {ret_list}")
             last = to_use
 
         print(ret_list)
@@ -30,4 +34,4 @@ class Solution:
     
 if __name__ == "__main__":
     sol = Solution()
-    sol.reorganizeString("sfffp")
+    sol.reorganizeString("aaab")
